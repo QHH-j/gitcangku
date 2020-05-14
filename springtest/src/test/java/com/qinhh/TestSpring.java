4414cafe479fb26ae94f0d912ae54d6526e18d3b
qinhh <1141541132@qq.com>
2020-05-14 18:51:06 +0800

    第一次使用git  提交一下项目

---
+++ springtest/src/test/java/com/qinhh/TestSpring.java
@@ -0,0 +1,26 @@
+package com.qinhh;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import java.applet.AppletContext;
+
+/**
+ * @Author qinhh
+ * @Date 2020/5/14 18:02
+ * @Version 1.0
+ */
+public class TestSpring {
+
+    @Test
+    public void  test(){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        Person person =  ctx.getBean(Person.class);
+        System.out.println(person);
+
+
+    }
+
+
+}
